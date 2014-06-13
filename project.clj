961cbab48e97cfcaf989284fa5c955d5ca6e24dd
Michael Klishin <michael@clojurewerkz.org>
2014-06-13 19:51:57 +0400

    1.0.0-beta3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/mold "1.0.0-beta3-SNAPSHOT"
+(defproject clojurewerkz/mold "1.0.0-beta3"
   :description "Clojure library for working with CloudFoundry services (primarily Cloud Controller)"
   :dependencies [[org.clojure/clojure                      "1.6.0"]
                  [org.cloudfoundry/cloudfoundry-client-lib "1.0.3"]
