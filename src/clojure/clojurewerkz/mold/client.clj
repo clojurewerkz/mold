6cec35040884db0ece072634a2d8b5eef23b55d0
Michael Klishin <michael@clojurewerkz.org>
2014-07-16 15:51:54 +0400

    Make client/make-client and /info accept optional HTTP client options

--- src/clojure/clojurewerkz/mold/client.clj
+++ src/clojure/clojurewerkz/mold/client.clj
@@ -43,16 +43,22 @@
      ^OAuth2AccessToken      oauth-token])
 
 (defn info
-  [^String url]
-  (let [{:keys [body]} (http/get (maybe-append url "/v2/info") {:throw-exceptions true :accept :json})]
-    (json/parse-string body false)))
+  ([^String url]
+     (info url {}))
+  ([^String url http-opts]
+     (let [opts           (merge {:throw-exceptions true :accept :json}
+                                 http-opts)
+           {:keys [body]} (http/get (maybe-append url "/v2/info") opts)]
+       (json/parse-string body false))))
 
 (defn ^String get-authorization-endpoint
   "Infers and returns authorization (UAA API) endpoint used by
    the provided Cloud Controller URL."
-  [^String url]
-  (let [m              (info url)]
-    (get m "authorization_endpoint")))
+  ([^String url]
+     (get-authorization-endpoint url {}))
+  ([^String url http-opts]
+     (let [m              (info url http-opts)]
+       (get m "authorization_endpoint"))))
 
 (defn ^clojurewerkz.mold.client.CFClient make-client
   ([url]
@@ -61,26 +67,28 @@
                 http-proxy-configuration
                 trust-self-signed-certs?
                 organization
-                space] :or {credentials {}
-                            trust-self-signed-certs? false}}]
+                space http-opts] :or {credentials {}
+                                      trust-self-signed-certs? false
+                                      http-opts   {}}}]
      (let [url'         (to-url url)
            credentials' (cnv/->credentials credentials)
            proxy-cfg    (cnv/->http-proxy-configuration http-proxy-configuration)
            om           (org.codehaus.jackson.map.ObjectMapper.)
            ru           (RestUtil.)
            rt           (.createRestTemplate ru proxy-cfg trust-self-signed-certs?)
-           oac          (oa2/make-oauth-client (get-authorization-endpoint url)
+           aue          (get-authorization-endpoint url http-opts)
+           oac          (oa2/make-oauth-client aue
                                                trust-self-signed-certs?
                                                proxy-cfg)
            lc           (LoggregatorClient. trust-self-signed-certs?)
            ccci         (if (and (nil? organization)
                                  (nil? space))
                           (CloudControllerClientImpl. url'
-                                                    rt oac lc
-                                                    credentials' nil)
+                                                      rt oac lc
+                                                      credentials' nil)
                           (CloudControllerClientImpl. url'
-                                                    rt oac lc
-                                                    credentials' organization space))
+                                                      rt oac lc
+                                                      credentials' organization space))
            cfc          (CloudFoundryClient. ccci)
            token        (.login cfc)]
        (CFClient. cfc url oac token))))
